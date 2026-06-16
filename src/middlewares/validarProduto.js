67209a2f26d4df96bbce2bb4ebd60e3dcf5745e9
NILTON HORST NETO <nilton_horst-neto@estudante.sesisenai.org.br>
2026-06-16 09:59:01 -0300

    restaura api

--- src/middlewares/validarProduto.js
+++ src/middlewares/validarProduto.js
@@ -1,9 +1,15 @@
 export function validarProduto(req, res, next) {
   const { nome, preco } = req.body
 
-  if (!nome || preco == null) {
+  if (!nome || preco === undefined) {
     return res.status(400).json({
       mensagem: "Nome e preço são obrigatórios"
+    })
+  }
+
+  if (preco <= 0) {
+    return res.status(400).json({
+      mensagem: "Preço deve ser maior que zero"
     })
   }
 
